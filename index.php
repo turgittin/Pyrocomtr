ae78936c1eb7e6bdc3a7d895db36678a4d0c3802
turgittin <turgay.oz@gmail.com>
2015-01-08 12:45:52 +0200

    e-bülten box live!
    düzeltme ile temiz görünüme geldi agacım.

--- index.php
+++ index.php
@@ -293,12 +293,12 @@ $img = $cerezler[$dk%count($cerezler)];
     <div class="newbg footerTopSection">
         <div class="container">
             <div class="col-sm-8">
-                <p style="color:#fff;font-size:20px;margin: 4px 0 10px 0px;">Size güncel haber ve özel fırsat bültenleri göndermemizi ister misiniz?</p>
+                <p style="color:#fff;font-size:20px;margin: 3px 0px 0px 0px;">Size güncel haber ve özel fırsat bültenleri göndermemizi ister misiniz?</p>
             </div>
             <div class="col-sm-4">
                 <form>
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="lorem@gmail.com" required>
+                        <input type="text" class="form-control" placeholder="hey@pyro.com.tr" required>
                         <div class="input-group-btn">
                             <button class="btn btn-default"><i class="glyphicon glyphicon-send"></i></button>
                         </div>
