ab4191df8dd69f9deeb6232ec554459d43e84e72
Erdi <erdi.doqan@gmail.com>
2015-01-16 16:20:51 +0100

    htaccess ayarlandi

--- nav.php
+++ nav.php
@@ -12,13 +12,13 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
             <li>
-                <a href="./neler-yapiyoruz.php">NELER YAPIYORUZ?</a>
+                <a href="neler-yapiyoruz">NELER YAPIYORUZ?</a>
             </li>
             <li>
-                <a href="projeler">PROJELER</a>
+                <a href="proje-ler">PROJELER</a>
             </li>
             <li>
-                <a href="./sosyal-duyarlilik.php">SOSYAL DUYARLILIK</a>
+                <a href="sosyal-duyarlilik">SOSYAL DUYARLILIK</a>
             </li>
             <li>
                 <a href="#">BLOG</a>
